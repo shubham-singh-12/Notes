56fe38c0216fb173d0600882f737d5127f1c0d52
shubham-singh-12 <shubham1272singh@gmail.com>
2026-05-23 17:46:37 +0530

    Added - JS Notes, Ways to write JS, Variables & Values, Datatypes (Primitive Only), Concatenation, Type Coercion

---
+++ JS/Thapa_Technical/Data Types/concatenation.js
@@ -0,0 +1,11 @@
+// ================================================================= CONCATENATION IN JS =================================================================
+
+var userNumber_1 = 5;
+var userNumber_2 = 4;
+var total = userNumber_1 + userNumber_2;
+console.log(total)                          // 9
+
+var userFirstName = "Shubham";
+var userLastName = "Singh";
+var userFullName = userFirstName + " " + userLastName;
+console.log(userFullName);                 // Shubham Singh
---
+++ JS/Thapa_Technical/Data Types/type_coercion.js
@@ -0,0 +1,25 @@
+// ================================================================= TYPE COERCION IN JS =================================================================
+
+console.log("5" + 5);                   // 55
+
+console.log("5" - 5);                   // 0
+
+console.log(10 + "20");                 // 1020
+
+console.log(9 - "5");                   // 4
+
+console.log("Java" + "Script");         // JavaScript
+
+console.log(" " + " ");                 // 
+
+console.log(" " + 0);                   //  0
+
+console.log("Shubham" - "Singh");       // NaN
+
+console.log(true + true);               // 2
+
+console.log(true + false);              // 1
+
+console.log(false + true);              // 1
+
+console.log(false - true);              // -1
