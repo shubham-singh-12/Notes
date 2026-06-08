c40785c2de6c64293e431cbaa53f697def9d8c5d
shubham-singh-12 <shubham1272singh@gmail.com>
2026-06-08 10:15:57 +0530

    Added - Update Arrays, Add String Folder, Update notes (Include String Notes)

---
+++ JS/Thapa_Technical/Strings/strings.js
@@ -0,0 +1,10 @@
+// ======================================================================= STRINGS =======================================================================
+
+
+let firstName = 'Shubham';              // Single Quotes (' ')
+let lastName = "Singh";                 // Double Quotes (" ")
+let fullName = `Shubham Singh`;         // Backticks (` `)
+
+console.log(firstName);                 // Shubham
+console.log(lastName);                  // Singh
+console.log(fullName);                  // Shubham Singh
