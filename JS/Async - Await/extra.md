e77ee05945866351d5b30f5293e25622defefd96
shubham-singh-12 <shubham1272singh@gmail.com>
2026-07-08 04:59:41 +0530

    Add Object methods along with notes

---
+++ JS/Async - Await/extra.md
@@ -0,0 +1,2 @@
+Promise {<pending>}   ← data not ready yet ⚠️
+(3 seconds later — promise resolves silently, no one uses it)
--- JS/Objects/methods.js
+++ JS/Objects/methods.js
@@ -1,33 +1,107 @@
-// =========================================================== METHOD (Function inside Object) ===========================================================
+// ======================================================================== METHOD ========================================================================
 
-// // 1. Normal function inside Object
-// let user = {
-//     name: "Alice",
-//     age: 25,
-
-//     // Method inside object
-//     greet: function () {
-//         console.log(`Hello World!!!`);
-//     },
+// ======================= Object.keys(obj) =======================
+// let obj = {
+//     name: "Shubham",
+//     age: 24,
 // };
 
-// user.greet();                   // Hello World!!!
+// let keys = Object.keys(obj);
+// console.log(keys);                  // [ 'name', 'age' ]
 
 
 
 
 
-// ================================================================
-// // 2. Return something from function
-
-// let user = {
-//     name: "Alice",
-//     age: 25,
-//     greet: function () {
-//         return 20;
-//     }
+// ========================================================================================================================================================
+// ====================== Object.values(obj) ======================
+// let obj = {
+//     name: "Shubham",
+//     age: 24,
 // };
 
-// const functionReturn = user.greet();
+// let values = Object.values(obj);
+// console.log(values);                    // [ 'Shubham', 24 ]
 
-// console.log(functionReturn);                    // 20
+
+
+
+
+// ========================================================================================================================================================
+// ===================== Object.entries(obj) ======================
+// let obj = {
+//     name: "Shubham",
+//     age: 24,
+// };
+
+// let entries = Object.entries(obj);
+// console.log(entries);
+// // [ [ 'name', 'Shubham' ], [ 'age', 24 ] ]
+
+
+
+
+
+// ========================================================================================================================================================
+// ================ Object.assign(target, source) =================
+// let obj_1 = {
+//     name:"Shubham",
+//     age: 24,
+// };
+
+// let obj_2 = {}
+// Object.assign(obj_2, obj_1);
+
+// console.log(obj_2);                 // { name: 'Shubham', age: 24 }
+
+
+
+
+
+// ========================================================================================================================================================
+// ====================== Object.freeze(obj) ======================
+// let obj = {
+//     name: "Shubham",
+//     age: 24,
+// };
+
+// Object.freeze(obj);
+
+// obj.city = "Delhi";         // Not Applied
+// delete obj.name;            // Not Applied
+
+// console.log(obj);                   // { name: 'Shubham', age: 24 }
+
+
+
+
+
+// ========================================================================================================================================================
+// ======================= Object.seal(obj) =======================
+// let obj = {
+//     name: "Shubham",
+//     age: 24,
+// };
+
+// Object.seal(obj);
+
+// obj.city = "Delhi";             // Operation Not Performed
+// delete obj.name;                // Operation Not Performed
+// obj.name = "Shivam";            // Operation Done
+
+// console.log(obj);               // { name: 'Shivam', age: 24 }
+
+
+
+
+
+// ========================================================================================================================================================
+// ================= ObjName.hasOwnProperty(key) ==================
+// let obj = {
+//     name: "Shubham",
+//     age: 24,
+// };
+
+// console.log(obj.hasOwnProperty("age"));                 // true
+// console.log(obj.hasOwnProperty("city"));                // false
+// console.log(obj.hasOwnProperty("Shubham"));             // false
