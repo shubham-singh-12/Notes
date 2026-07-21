2dff1b2759039ee33c6e1218bedbf7bd88e7ba1c
shubham-singh-12 <shubham1272singh@gmail.com>
2026-07-21 19:15:42 +0530

    Delete Extra JAVA Folder

--- Java/Lecture_4/src/Semicolon.java
+++
@@ -1,8 +0,0 @@
-// ============================================================================== SEMICOLON ==============================================================================
-
-void main () {
-    System.out.println("Hello");        // Semicolon ends this statement
-    int x = 5;                          // Semicolon ends this statement
-
-    int y = 6                           // java: ';' expected
-}
--- Java/Lecture_6_Data_types_and_Type_Casting/src/DataType.java
+++
@@ -1,9 +0,0 @@
-// ============================================================================== DATA TYPE ===============================================================================
-static void main() {
-    int age = 5;
-}
-
-
-// "int" = Data Type
-// tells us --> Stores INTEGER type data
-// tells us --> Takes 4 bytes of memory
