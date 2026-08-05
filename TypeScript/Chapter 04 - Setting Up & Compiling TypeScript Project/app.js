469eae928b2e0b1d5e119652c67bb9e61c04b675
shubham-singh-12 <shubham1272singh@gmail.com>
2026-08-06 01:01:17 +0530

    Typescript Started

---
+++ TypeScript/Chapter 04 - Setting Up & Compiling TypeScript Project/app.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 12;
+var b = "Shubham";
+function abcd() {
+    var c = 12;
+    var d = 23;
+    return c + d;
+}
+;
+console.log(abcd());
+//# sourceMappingURL=app.js.map
---
+++ TypeScript/Chapter 04 - Setting Up & Compiling TypeScript Project/app.ts
@@ -0,0 +1,10 @@
+var a = 12;
+var b = "Shubham";
+
+function abcd() {
+    var c = 12;
+    var d = 23;
+    return c + d;
+};
+
+console.log(abcd());
